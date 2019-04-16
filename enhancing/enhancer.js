78c8396f1c2a8933d5f4f7b634ebc22ab4351dc6
lydiathornton <lydia.d.thornton@gmail.com>
2019-04-16 08:12:00 -0700

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,18 +2,32 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
+  if (item.enhancement === 20) {
+    item.enhancement = 20;
+  } else {
+    item.enhancement += 1;
+  }
+
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability -= 5;
+  } else if (item.enhancement > 14 && item.enhancement < 17) {
+    item.durability -= 10;
+  } else {
+    item.enhancement -= 1;
+  }
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require("./enhancer.js");
 // test away!
+
+it("should increase enhancement by 1", () => {
+  expect(succeed({ name: "Weapon", enhancement: 15, durability: 98 })).toEqual({
+    name: "Weapon",
+    enhancement: 16,
+    durability: 98
+  });
+});
+
+it("Should return the item with a replenished durability of 100", () => {
+  expect(repair({ name: "Hammer", enhancement: 13, durability: 20 })).toEqual({
+    name: "Hammer",
+    enhancement: 13,
+    durability: 100
+  });
+});
